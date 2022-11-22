5f3a115eb5b97b9cbe72bf55090fcc7948bd79f5
Pablo Belfort <pablobelfort@gmail.com>
2022-11-22 14:09:48 -0300

    bug fixing

--- src/app.js
+++ src/app.js
@@ -32,6 +32,6 @@ export const transactionSchema = joi.object({
 router.use(accountRouter)
 router.use(transactionsRouter)
 
-app.listen(5000, () => {
-    console.log('running on port:5000')
+app.listen(process.env.PORT, () => {
+    console.log('running on port' + process.env.PORT)
 })
